98262a825d84d55d45b4c0fa294b87762d7a7b88
Dmitriy Shidlovskij <91601631+Xubabyb@users.noreply.github.com>
2022-02-26 01:13:58 +0300

    Update Exercise03.java

--- object_string/Exercise03.java
+++ object_string/Exercise03.java
@@ -6,60 +6,18 @@
 package by.jonline.modul03.object_string;
 
 public class Exercise03 {
-
 	public static void main(String[] args) {
+ 		String text = "Madam  i'm Adam";
+        	System.out.println(isPalindrome(text));
+    	}
 
-		String[] strMass = new String[] { " Sum   mussum Mus"/* Латынь */, "Madam  i'm Adam", "Could you help me" };
+    	public static boolean isPalindrome(String text) {
+        	
+		text = text.replaceAll("[^a-zA-Z0-9]", "");
 
-		for (String str : strMass) {
+        	String reverse = new StringBuilder(text).reverse().toString();
 
-			String result = checkPalindrom(str) ? " - палиндром" : " - не палиндром";
-
-			System.out.println(str + result);
-		}
-
-	}
-
-	public static boolean checkPalindrom(String s) {
-
-		boolean bool = true;
-
-		s = deleteCharacter(s);
-
-		for (int i = 0; i < s.length() / 2; i++) {
-
-			String a = String.valueOf(s.charAt(i));
-
-			String b = String.valueOf(s.charAt(s.length() - 1 - i));
-
-			if (!a.equalsIgnoreCase(b)) {
-
-				bool = false;
-
-				//System.out.println(i);
-
-				break;
-			}
-		}
-
-		return bool;
-	}
-
-	public static String deleteCharacter(String s) {
-
-		StringBuilder x = new StringBuilder();
-
-//		char[] massCh = s.toCharArray();
-
-		for (int i = 0; i < s.length(); i++) {
-
-			if (s.charAt(i) != ' ' && s.charAt(i) != '\'') {
-
-				x.append(s.charAt(i));
-			}
-		}
-		s = x.toString();
-
-		return s.trim();
-	}
+        	return text.equalsIgnoreCase(reverse);
+    }
+		
 }
